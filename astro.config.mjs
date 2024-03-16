88b99663f26077a0b28eac88edc8afc1cd07061f
SeungWoo <wjknn3123@gmail.com>
2024-03-17 01:21:40 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+import tailwindIntegration from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    mdx(),
+    sitemap(),
+  ],
+});
