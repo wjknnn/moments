23fb9a6045b24838558578cd4ce680a70a95dd4f
SeungWoo <wjknn3123@gmail.com>
2024-03-23 10:15:29 +0900

    🔧 Add :: tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
-import tailwindIntegration from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [
-    tailwind({ applyBaseStyles: false }),
+    tailwind({ applyBaseStyles: false, nesting: true }),
     react(),
     mdx(),
     sitemap(),
