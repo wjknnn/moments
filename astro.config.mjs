a9039be6f186ce4066d4c5581dc3113d3405bc5f
SeungWoo <wjknn3123@gmail.com>
2024-06-28 16:26:58 +0900

    🔨 Add :: Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://moments-omega.vercel.app',
   integrations: [
     starlight({
       title: 'Moments',
